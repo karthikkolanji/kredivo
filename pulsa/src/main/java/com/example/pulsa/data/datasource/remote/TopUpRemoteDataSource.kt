b37dfaf24fea4a9b1a23339400fefb4f191aef52
karthikkolanji <karthik.kolanji90@gmail.com>
2024-02-26 23:10:22 +0530

    Refactor: uses io thread for api service

--- pulsa/src/main/java/com/example/pulsa/data/datasource/remote/TopUpRemoteDataSource.kt
+++ pulsa/src/main/java/com/example/pulsa/data/datasource/remote/TopUpRemoteDataSource.kt
@@ -1,10 +1,14 @@
 package com.example.pulsa.data.datasource.remote
 
+import com.example.core.di.DispatcherProvider
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class TopUpRemoteDataSource @Inject constructor(private val apiService: ApiService) {
-
-    suspend fun getPulsaPlans()=apiService.getPlans()
-    suspend fun makePayment()=apiService.makePayment()
-    suspend fun getVoucher()=apiService.getVoucher()
+class TopUpRemoteDataSource @Inject constructor(
+    private val apiService: ApiService,
+    private val dispatcherProvider: DispatcherProvider
+) {
+    suspend fun getPulsaPlans() = withContext(dispatcherProvider.io()) { apiService.getPlans() }
+    suspend fun makePayment() = withContext(dispatcherProvider.io()) { apiService.makePayment() }
+    suspend fun getVoucher() = withContext(dispatcherProvider.io()) { apiService.getVoucher() }
 }
